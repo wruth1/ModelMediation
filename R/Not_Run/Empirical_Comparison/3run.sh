2b03d900dedb872cf8a872acb35661a9cff233b1
wruth1 <wruth@sfu.ca>
2024-03-13 21:30:13 -0700

    Bootstrap results for coverage probability study

---
+++ R/Not_Run/Empirical_Comparison/3run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --time=6:0:0
+#SBATCH --ntasks=48
+#SBATCH --mem-per-cpu=2000M
+#SBATCH --array=25-29
+#SBATCH --output="./output/%A_%a.out"
+#SBATCH --mail-user=wruth@sfu.ca
+#SBATCH --mail-type=END
+
+module load gcc r
+
+export NODESLIST=$(echo $(srun hostname | cut -f 1 -d '.'))
+Rscript One_Setting.R $SLURM_ARRAY_TASK_ID
---
+++ R/Not_Run/Empirical_Comparison/c.R
@@ -0,0 +1,12 @@
+
+
+
+
+parallel::stopCluster(my_cluster)
+
+
+
+cat("\n")
+runtime = tictoc::toc()
+
+# save(runtime, file = external_runtime_prefix)

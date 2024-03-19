17bbcd4d5bf892a103718c584f864e6eb6888cfd
wruth1 <wruth@sfu.ca>
2024-03-19 10:12:04 -0700

    Clean-up old results and add new ones

--- R/Not_Run/Empirical_Comparison/3run.sh
+++ R/Not_Run/Empirical_Comparison/3run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --time=3:0:0
+#SBATCH --time=6:0:0
 #SBATCH --ntasks=48
 #SBATCH --mem-per-cpu=2000M
-#SBATCH --array=17-25
+#SBATCH --array=18-25
 #SBATCH --output="./output/%A_%a.out"
 #SBATCH --mail-user=wruth@sfu.ca
 #SBATCH --mail-type=END

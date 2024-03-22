672231dc44657787df02336116fc363ff191b2e2
wruth1 <wruth@sfu.ca>
2024-03-22 11:05:19 -0700

    Test job-level parallelization across bootstrap and Monte Carlo replicates

--- R/Not_Run/Empirical_Comparison/run.sh
+++ R/Not_Run/Empirical_Comparison/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=14:0:0
+#SBATCH --time=6:0:0
 #SBATCH --ntasks=2
 #SBATCH --mem-per-cpu=2000M
 #SBATCH --array=1
@@ -10,4 +10,4 @@
 module load gcc r
 
 export NODESLIST=$(echo $(srun hostname | cut -f 1 -d '.'))
-Rscript Par-One_Setting.R
+Rscript Par-One_Setting.R $SLURM_ARRAY_TASK_ID

719c3bdd2a718ccd56a02a6db0422202ea4e4168
wruth1 <wruth@sfu.ca>
2024-03-27 09:08:02 -0700

    Sampling dist and bootstrap dist for fitted mediation effects

--- R/Not_Run/Empirical_Comparison/1run.sh
+++ R/Not_Run/Empirical_Comparison/1run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --time=1:0:0
+#SBATCH --time=2:0:0
 #SBATCH --ntasks=48
 #SBATCH --mem-per-cpu=2000M
 # #SBATCH --array=1-9
@@ -11,4 +11,4 @@
 module load gcc r
 
 export NODESLIST=$(echo $(srun hostname | cut -f 1 -d '.'))
-Rscript One_Setting.R $SLURM_ARRAY_TASK_ID
+Rscript get_EDF_med_effs.R $SLURM_ARRAY_TASK_ID
--- R/Not_Run/Empirical_Comparison/run.sh
+++ R/Not_Run/Empirical_Comparison/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --time=6:0:0
-#SBATCH --ntasks=2
+#SBATCH --ntasks=48
 #SBATCH --mem-per-cpu=2000M
-#SBATCH --array=1
+#SBATCH --array=1-3
 #SBATCH --output="./output/%A_%a.out"
 #SBATCH --mail-user=wruth@sfu.ca
 #SBATCH --mail-type=END
@@ -10,4 +10,4 @@
 module load gcc r
 
 export NODESLIST=$(echo $(srun hostname | cut -f 1 -d '.'))
-Rscript Par-One_Setting.R $SLURM_ARRAY_TASK_ID
+Rscript copy_Par-One_Setting.R $SLURM_ARRAY_TASK_ID
